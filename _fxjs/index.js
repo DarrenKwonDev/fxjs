4e6d137bfc022f7b49dd70ba557052deca50c092
DarrenKwonDev <anatomy1545@gmail.com>
2021-05-25 02:05:53 +0900

    currify, go, pipe

--- _fxjs/index.js
+++ _fxjs/index.js
@@ -2,25 +2,37 @@
 
 export const isIterable = a => a && a[Symbol.iterator];
 
-export const map = (f, iter) => {
+//** currify, multipleCurrify */
+// add(1, 2)를 add(1)(2) 꼴로 변경 가능.
+// 함수 내 인자가 2개 이상일 경우(_length > 0) 첫 인자를 함수에 넣고 재귀적으로 다시 평가 반복. 1개만 남을 때까지 재귀적으로 돌리기
+export const currify = f => (a, ..._) => (_.length ? f(a, ..._) : (..._) => f(a, ..._));
+
+// add(1, 2, 3)을 add(1, 2)(3), add(1)(2)(3) 꼴로 변경 가능
+export const multipleCurrify = f =>
+  function curried(...args) {
+    return args.length >= f.length ? f(...args) : (...args2) => curried(...args, ...args2);
+  };
+
+//** eager valuation functions */
+export const map = currify((f, iter) => {
   if (!isIterable(iter)) throw Error('not iterable');
   let arr = [];
   for (const value of iter) {
     arr.push(f(value));
   }
   return arr;
-};
+});
 
-export const filter = (f, iter) => {
+export const filter = currify((f, iter) => {
   if (!isIterable(iter)) throw Error('not iterable');
   let arr = [];
   for (const value of iter) {
     if (f(value)) arr.push(value);
   }
   return arr;
-};
+});
 
-export const reduce = (f, acc, iter) => {
+export const reduce = currify((f, acc, iter) => {
   // 인자가 2개만 주어졌을 때, well-formed iterable을 일단 한 번 next로 소비한 후에 acc로 설정
   if (!iter) {
     iter = acc[Symbol.iterator]();
@@ -30,6 +42,15 @@ export const reduce = (f, acc, iter) => {
     acc = f(acc, value);
   }
   return acc;
-};
+});
+
+//** go and pipe functions */
+export const go = (...args) => reduce((a, f) => f(a), args);
+
+// 함수 연속 실행. 이후 커링으로 전달된 변수만 적용
+export const oldPipe = (...fs) => a => go(a, ...fs);
+
+// 첫 함수에 커링으로 전달된 변수 적용한 후 순차적으로 함수 연속 실행
+export const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs);
 
 // lazy
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { filter, map, reduce } from './_fxjs';
+import { filter, go, map, pipe, reduce, oldPipe, currify } from './_fxjs';
 
 const products = [
   { name: '반팔티', price: 15000, quantity: 1 },
@@ -11,3 +11,43 @@ const products = [
 // console.log(map(a => a.name, products));
 // console.log(filter(a => a.price > 20000, products));
 // console.log(reduce((acc, cur) => acc + cur, [1, 2, 3]));
+
+// go(
+//   0, // 내부적으로 reduce의 첫 acc가 됨.
+//   a => a + 1, // reduce 다음부터 f를 전달하여 중첩하여 적용
+//   a => a + 10,
+//   console.log
+// );
+
+// go(
+//   products, // 내부적으로 reduce의 첫 acc가 됨.
+//   products => filter(p => p.price > 20000, products), // reduce 다음부터 f를 전달하여 중첩하여 적용
+//   products => map(p => p.price, products),
+//   products => reduce((acc, cur) => acc + cur, products),
+//   console.log
+// );
+
+// const oldPipeFunc = oldPipe(
+//   a => a + 1,
+//   a => a + 10,
+//   console.log
+// )(0);
+
+// const pipeFunc = pipe(
+//   (a, b) => a + b,
+//   a => a + 1,
+//   a => a + 10,
+//   console.log
+// )(1, 2);
+
+// const mult = currify((a, b) => a * b);
+// console.log(mult(3)(5)); // 15
+
+// map, filter, reduce를 currify함.
+// go(
+//   products,
+//   filter(p => p.price > 20000),
+//   map(p => p.price),
+//   reduce((acc, cur) => acc + cur),
+//   console.log
+// );
